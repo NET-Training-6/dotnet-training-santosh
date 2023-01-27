e106b0cad6daff23d82f3711ca1ed6b3ed35dc86
santosraj38 <santosraj38@gmail.com>
2023-01-27 18:34:40 +0545

    =Multiplication Table

---
+++ WebDevelopment/HelloWebApp/WebRazorPages/Pages/MultiplicationTable.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebRazorPages.Pages
+{
+    public class MultiplicationTableModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
